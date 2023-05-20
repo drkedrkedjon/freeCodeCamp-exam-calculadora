1e6d4ef51f121194a5a6240e1b39ebcdbce656b5
Sasa Savic Perusina <sasa@bubulazy.com>
2023-05-20 14:13:52 +0100

    Cycle timers done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(1);
+  const [sessionLength, setSessionLength] = useState(1);
   const [timeLeft, setTimeLeft] = useState("25:00");
+  const [isStartClock, setIsStartClock] = useState(true);
+  const [timerLabel, setTimerLabel] = useState("Session");
+  const [isBreak, setIsBreak] = useState(false);
+  // console.log(isBreak);
 
   function handleResetClock() {
-    setBreakLength(5);
-    setSessionLength(25);
+    setBreakLength(1);
+    setSessionLength(1);
     setTimeLeft("25:00");
+    sessionLengthSeconds.current = sessionLength * 60;
+    setTimerLabel("Session");
+    setIsBreak(false);
   }
   function handleClockLengths(e) {
     switch (e.target.id) {
@@ -34,6 +41,43 @@ function App() {
         break;
     }
   }
+
+  const interval = useRef(null);
+  const sessionLengthSeconds = useRef(null);
+  function startClock() {
+    sessionLengthSeconds.current = sessionLength * 60;
+    let isBreakTrue = isBreak;
+
+    interval.current = setInterval(function () {
+      let min = Math.floor(sessionLengthSeconds.current / 60);
+      let sec = sessionLengthSeconds.current % 60;
+      setTimeLeft(min + ":" + sec);
+      sessionLengthSeconds.current -= 1;
+
+      if (sessionLengthSeconds.current < 0 && !isBreakTrue) {
+        setTimerLabel("Break");
+        setIsBreak(true);
+        sessionLengthSeconds.current = breakLength * 60;
+        isBreakTrue = true;
+        console.log("cAmbio a break");
+      }
+
+      if (sessionLengthSeconds.current < 0 && isBreakTrue) {
+        setTimerLabel("Session");
+        setIsBreak(false);
+        sessionLengthSeconds.current = sessionLength * 60;
+        isBreakTrue = false;
+        console.log("cAmbio a Session");
+      }
+    }, 1000);
+    setIsStartClock(false);
+  }
+
+  function stopClock() {
+    clearInterval(interval.current);
+    setIsStartClock(true);
+  }
+
   return (
     <main className="container">
       <h1>25 + 5 Clock</h1>
@@ -67,12 +111,20 @@ function App() {
       </div>
 
       <div className="display">
-        <h2 id="timer-label">Session</h2>
+        <h2 id="timer-label">{timerLabel}</h2>
         <p id="time-left">{timeLeft}</p>
         <div className="display-controls">
-          <button id="start_stop">start</button>
+          {isStartClock ? (
+            <button onClick={startClock} id="start_stop">
+              Start
+            </button>
+          ) : (
+            <button onClick={stopClock} id="start_stop">
+              Stop
+            </button>
+          )}
           <button onClick={handleResetClock} id="reset">
-            reset
+            Reset
           </button>
         </div>
       </div>
